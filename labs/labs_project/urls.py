fae3c7394f0003648aab0787db2d328d39545ce2
Елизавета Меньщикова <elizavetamenshchikova@MacBook-Air-Elizaveta.local>
2022-05-17 16:24:25 +0300

    faulty urls2

--- labs/labs_project/urls.py
+++ labs/labs_project/urls.py
@@ -4,6 +4,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('labs_project.urls')),
-    path('laboratories/', include('laboratories.urls')),
+    path('', include('laboratories.urls')),
 ]
