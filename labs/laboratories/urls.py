8d3d729f4f07de2cf6399770977c136182c102f2
Елизавета Меньщикова <elizavetamenshchikova@MacBook-Air-Elizaveta.local>
2022-05-17 16:40:58 +0300

    ??

--- labs/laboratories/urls.py
+++ labs/laboratories/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.laboratories_home, name = 'laboratories_home'),
-    path('create', views.create, name = 'create')
+
 ]
