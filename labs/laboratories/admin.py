b197b8e575fb44c36a2f427ec59b7de1628586bf
Елизавета Меньщикова <elizavetamenshchikova@MacBook-Air-Elizaveta.local>
2022-05-17 13:51:25 +0300

    experiments added

--- labs/laboratories/admin.py
+++ labs/laboratories/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Lab, Expert
+from .models import Lab, Expert, Sample, Experiment
 
 admin.site.register(Lab)
 admin.site.register(Expert)
+admin.site.register(Sample)
+admin.site.register(Experiment)
--- labs/laboratories/models.py
+++ labs/laboratories/models.py
@@ -23,3 +23,28 @@ class Expert(models.Model):
     class Meta:
         verbose_name = 'Экспериментатор'
         verbose_name_plural = 'Экспериментаторы'
+
+
+class Sample(models.Model):
+    name = models.CharField('Наименование', max_length = 50)
+    code = models.CharField('Код образца', max_length = 10)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Образец'
+        verbose_name_plural = 'Образцы'
+
+
+class Experiment(models.Model):
+    lab_id = models.ForeignKey(Lab, on_delete=models.CASCADE)
+    sample_id = models.ForeignKey(Sample, on_delete=models.CASCADE)
+    expert_id = models.ManyToManyField(Expert)
+
+    def __str__(self):
+        return self.lab_id.username + ' - ' + self.sample_id.username
+
+    class Meta:
+        verbose_name = 'Эксперимент'
+        verbose_name_plural = 'Эксперименты'
